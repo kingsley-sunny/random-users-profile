482705611c8e934e220c9234f942ead7ee3cbfe8
kingsley-sunny <ezeobisunny51@gmail.com>
2022-10-09 00:20:46 +0100

    finally made it responsive

--- src/components/dark-mode-button/darkModeBtn.jsx
+++ src/components/dark-mode-button/darkModeBtn.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./darkModeBtn.css";
 
-const DarkModeBtn = (props) => {
-  return (
-    <div style={{ display: "flex", gap: "2px", alignItems: "center" }}>
-      <h2 style={{ textTransform: "capitalize" }}>{props.mode}</h2>
-      <div className="button-container" onClick={props.toggleDarkMode}>
-        <div className={`button-child1 ${props.effect.one}`}></div>
-        <div className={`button-child2 ${props.effect.two}`}></div>
-      </div>
-    </div>
-  );
+const DarkModeBtn = props => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginTop: 20,
+            }}
+        >
+            <h2 style={{ textTransform: "capitalize", marginRight: 10 }}>{props.mode}</h2>
+            <div className='button-container' onClick={props.toggleDarkMode}>
+                <div className={`button-child1 ${props.effect.one}`}></div>
+                <div className={`button-child2 ${props.effect.two}`}></div>
+            </div>
+        </div>
+    );
 };
 
 export default DarkModeBtn;
